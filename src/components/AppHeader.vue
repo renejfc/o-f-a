7fac4b468caddf3453977aea7936ae64fab26086
renejfc <renejfc@gmail.com>
2021-01-27 19:22:38 +0100

    added header styles

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,11 @@
 <template>
-  <header>
-    <img src="" alt="" />
-    <p>{{ hd.storeName }}</p>
-    <h1>{{ hd.title }}</h1>
-    <h4>{{ hd.subtitle }}</h4>
+  <header class="flex flex-col">
+    <figure class="flex justify-between items-center">
+      <img src="@/assets/logo.png" alt="" class="w-9 h-10 object-contain"/>
+      <figcaption class="text-lg font-semibold">{{ hd.storeName }}</figcaption>
+    </figure>
+    <h1 class="text-xl font-semibold">{{ hd.title }}</h1>
+    <h4 class="text-base text-gray-500">{{ hd.subtitle }}</h4>
   </header>
 </template>
 
